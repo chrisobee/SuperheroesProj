daf9d2db485b592a38c789f9a073e2acca8ad2bf
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-20 14:04:45 -0500

    Added Create view and functioning HttpPost Create

--- Controllers/SuperheroController.cs
+++ Controllers/SuperheroController.cs
@@ -4,25 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
+using Superheroes.Models;
 
 namespace Superheroes.Controllers
 {
     public class SuperheroController : Controller
     {
-        // GET: Superhero
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public SuperheroController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        // GET: Superhero
+        public IActionResult Index()
+        {
+            var superheroes = _context.Superheroes.ToList();
+
+            return View(superheroes);
         }
 
         // GET: Superhero/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
         // GET: Superhero/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -30,12 +40,12 @@ namespace Superheroes.Controllers
         // POST: Superhero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Create(Superhero superhero)
         {
             try
             {
-                // TODO: Add insert logic here
-
+                _context.Superheroes.Add(superhero);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,7 +55,7 @@ namespace Superheroes.Controllers
         }
 
         // GET: Superhero/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int id)
         {
             return View();
         }
@@ -53,7 +63,7 @@ namespace Superheroes.Controllers
         // POST: Superhero/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(int id, IFormCollection collection)
         {
             try
             {
@@ -68,7 +78,7 @@ namespace Superheroes.Controllers
         }
 
         // GET: Superhero/Delete/5
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int id)
         {
             return View();
         }
@@ -76,7 +86,7 @@ namespace Superheroes.Controllers
         // POST: Superhero/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
